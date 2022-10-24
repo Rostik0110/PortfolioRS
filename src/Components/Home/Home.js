3efe160fa76427926a88205a404fe9dacf6deeca
Rostislav <rostikshafar@gmail.com>
2022-10-24 18:31:09 +0300

    header

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,15 +1,20 @@
-import React from 'react';
-import './Home.css';
-import Sidebar from '../Sidebar/Sidebar';
-import MainHomeContentSection from './MainHomeContentSection/MainHomeContentSection.js';
+import React from "react";
+import "./Home.css";
+import Header from "./Header/Header.js"
+import Content from "./Content/Content.js";
+import Footer from "./Footer/Footer.js";
 
-const Home = () => {
-    return(       
-            <div className="main-home-container">
-                <MainHomeContentSection />
-                <Sidebar />
-            </div>   
-    )
+const Home = () =>{
+    return(
+            <div className="home">
+                <div className="preloader">
+                </div>
+                <Header/>
+                <Content/>
+                <Footer/>
+            </div>
+        )
+
 }
 
 export default Home;
